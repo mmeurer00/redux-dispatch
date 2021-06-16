bc09cff350e3a3cb3d2fef4c586600a3a5364191
Maxine <maxinemeurer@yahoo.com>
2021-06-16 15:14:23 -0400

    commit tree 0f0ffc0dc4a56e677a566de2410f52e3aff9d9de

--- js/reducer.js
+++ js/reducer.js
@@ -1,11 +1,22 @@
-function changeState(state, action){
+function changeState(state = { count: 0 }, action) {
+
   switch (action.type) {
+
     case 'INCREASE_COUNT':
-      return {count: state.count + 1}
+      return { count: state.count + 1 }
+
     default:
       return state;
   }
+} 
+function dispatch(action){
+  state = changeState(state, action)
+  render()
 }
+
+function render(){
+  document.body.textContent = state.count
+} 
 
 let state = {count: 0}
 let action = {type: 'INCREASE_COUNT'}
